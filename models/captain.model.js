5717ecb69555701c08a8e14049db0ccf2de78580
yashwingahlawat <yashwingahlawat@gmail.com>
2025-03-30 23:02:25 +0530

    bug fix

--- models/captain.model.js
+++ models/captain.model.js
@@ -53,7 +53,7 @@ const captainSchema=new mongoose.Schema({
         vehicleType:{
             type:String,
             required:true,
-            enum:['car','motorcycle','auto']
+            enum:['car','moto','auto']
         },
     },
     location:{
--- routes/captain.routes.js
+++ routes/captain.routes.js
@@ -12,7 +12,7 @@ router.post('/register',[
     body('vehicle.color').isLength({min:3}).withMessage('Color must be atleast 3 characters long.'),
     body('vehicle.plate').isLength({min:3}).withMessage('Plate must be atleast 3 characters long.'),
     body('vehicle.capacity').isInt({min:1}).withMessage('Capacity must be atleast 1.'),
-    body('vehicle.vehicleType').isIn(['car','motorcycle','auto']).withMessage('Invalid Vehicle Type.')
+    body('vehicle.vehicleType').isIn(['car','moto','auto']).withMessage('Invalid Vehicle Type.')
 ],
 captainController.registerCaptain
 )
